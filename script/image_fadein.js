6c5f83d221fe73fc7cdece211e3156ed7710ea80
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-04 15:27:30 -0800

    Making issa-image placeholders fade-in

--- script/image_fadein.js
+++ script/image_fadein.js
@@ -1,24 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let posters = document.querySelectorAll("img.poster")
-    posters.forEach(poster => {
-        if (poster.complete) {
-            poster.classList.remove("loading")
+    let images = document.querySelectorAll("issa-image>img")
+    images.forEach(img => {
+        if (img.complete) {
+            img.classList.remove("loading")
         } else {
-            poster.addEventListener("load", (e) => {
-                poster.classList.remove("loading")
+            img.addEventListener("load", (e) => {
+                e.target.classList.remove("loading")
             });
         }
-        poster.addEventListener("error", (e) => {
+        img.addEventListener("error", (e) => {
             e.target.style.display = "none"
         });
-        poster.addEventListener("click", (e) => {
+        img.addEventListener("click", (e) => {
             e.target.classList.toggle("loading")
         })
-    })
-    let placeholders = document.querySelectorAll("img.placeholder")
-    placeholders.forEach(placeholder => {
-        placeholder.addEventListener("error", (e) => {
-            e.target.style.display = "none"
-        });
     })
 });
