58499ad3d66313f43b667ec03c4d507f069ffd07
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-02-24 15:17:30 -0800

    Refactoring rep-color usage to encode it into CSS (no SetTint required anymore) and allowing elements and page to set rep-color as needed

--- set_tint.go
+++
@@ -1,8 +0,0 @@
-package compton
-
-func SetTint(element Element, color string) {
-	element.SetAttribute(
-		"style",
-		"--tint-bg:color-mix(in display-p3,"+color+" var(--cma),var(--c-background))")
-
-}
