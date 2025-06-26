bc540c870d0d3f5d8bc402a5ffb2da255dc2a704
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-06-25 23:11:50 -0700

    Fixing a bug that was writing ever increasing attributes concatenation

--- attributes.go
+++ attributes.go
@@ -28,9 +28,9 @@ func (a *Attributes) Write(w io.Writer) error {
 	attrs := make([]string, 0, len(a.attributes))
 	for name, val := range a.attributes {
 		attrs = append(attrs, name+"='"+val+"'")
-		if _, err := w.Write([]byte(strings.Join(attrs, " "))); err != nil {
-			return err
-		}
+	}
+	if _, err := w.Write([]byte(strings.Join(attrs, " "))); err != nil {
+		return err
 	}
 	return nil
 }
