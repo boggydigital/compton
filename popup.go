afc170be3eefe793c17639327c8b5153e4993162
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-07-06 22:10:32 -0700

    Revert "Removing unused scripts"
    This reverts commit 5f1b52171a53f1a2f5614a50c1e80f9d27757c5e.

---
+++ popup.go
@@ -0,0 +1,32 @@
+package compton
+
+import (
+	_ "embed"
+	"github.com/boggydigital/compton/consts/compton_atoms"
+)
+
+var (
+	//go:embed "script/popup.js"
+	scriptPopup []byte
+)
+
+type PopupElement struct {
+	*BaseElement
+}
+
+func Attach(r Registrar, actor, target Element) *PopupElement {
+	pe := &PopupElement{
+		BaseElement: NewElement(contentMarkup(compton_atoms.Popup)),
+	}
+
+	if targetId := target.GetAttribute("id"); targetId != "" {
+		actor.SetAttribute("data-popup-target", targetId)
+		target.SetAttribute("data-popup", "hide")
+	}
+
+	r.RegisterStyles(DefaultStyle,
+		compton_atoms.StyleName(compton_atoms.Popup))
+	r.RegisterDeferrals(compton_atoms.ScriptName(compton_atoms.Popup), ScriptAsync(scriptPopup))
+
+	return pe
+}
