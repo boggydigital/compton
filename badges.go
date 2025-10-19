10057403390dc50e565c578e3e58df968dab6ef0
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-10-18 19:53:08 -0700

    Simplifying badges formatting and removing CSS hacks

--- badges.go
+++ badges.go
@@ -2,7 +2,6 @@ package compton
 
 import (
 	"github.com/boggydigital/compton/consts/color"
-	"github.com/boggydigital/compton/consts/font_weight"
 	"github.com/boggydigital/compton/consts/size"
 )
 
@@ -11,24 +10,13 @@ type FormattedBadge struct {
 	Icon  Symbol
 }
 
-func badgeContainer(r Registrar, c color.Color) *FspanElement {
-	return Fspan(r, "").
-		Width(size.Unset).
-		FontSize(size.XXSmall).
-		FontWeight(font_weight.Normal).
-		BorderRadius(size.XSmall).
-		BackgroundColor(color.Transparent).
-		ForegroundColor(c)
-}
-
-func BadgeIcon(r Registrar, icon Symbol, c color.Color) *FspanElement {
-	bc := badgeContainer(r, c)
-	bc.Append(SvgUse(r, icon))
-	return bc
+func BadgeIcon(r Registrar, icon Symbol, c color.Color) Element {
+	return SvgUse(r, icon).ForegroundColor(c)
 }
 
 func BadgeText(r Registrar, text string, c color.Color) *FspanElement {
-	btc := badgeContainer(r, c)
-	btc.Append(Text(text))
-	return btc
+	return Fspan(r, text).
+		Width(size.Unset).
+		BackgroundColor(color.Transparent).
+		ForegroundColor(c)
 }
